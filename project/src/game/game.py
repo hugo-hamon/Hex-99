8ba31eb375ae16015beaa2c9f692c69199de203f
Pierre <pierrep070225@gmail.com>
2024-01-31 14:38:14 +0100

    Check if winning

--- project/src/game/game.py
+++ project/src/game/game.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from ..config import Config
+from ..utils.neighbors import hex_neighbors
 from enum import Enum
 import numpy as np
 
@@ -63,7 +64,8 @@ class Game:
             self.board[move] = BoardState.PLAYER1
         else:
             self.board[move] = BoardState.PLAYER2
-
+        if (self.__is_winning_move(move)):
+            self.over = True
         self.switch_player()
 
     def reset(self) -> None:
@@ -78,7 +80,24 @@ class Game:
     # UTILS
     def __is_winning_move(self, move: tuple[int, int]) -> bool:
         """Return True if the given move is a winning move"""
-        raise NotImplementedError
+        if self.current_player == PlayerOrder.PLAYER1:
+            currentColor, player = BoardState.PLAYER1, 0
+            edges = (0, self.config.game.board_height - 1)
+        else:
+            currentColor, player = BoardState.PLAYER2, 1
+            edges = (0, self.config.game.board_width - 1)
+        # Check if the move is on the edge
+        if move[player] in edges:
+            return self.__isLinked(move, currentColor, player, edges[1])
+        # Else check if the move is connected to 2+ neighbors of the same color
+        # (Can't win otherwise)
+        multiple = False
+        for neighbor in hex_neighbors(move, (self.config.game.board_width, self.config.game.board_height)):
+            if self.board[neighbor] == currentColor:
+                if multiple:
+                    return self.__isLinked(move, currentColor, player, edges[1])
+                multiple = True
+        raise False
 
     def switch_player(self) -> None:
         """Switch the current player"""
@@ -87,3 +106,25 @@ class Game:
             PlayerOrder.PLAYER2: PlayerOrder.PLAYER1
         }
         self.current_player = other[self.current_player]
+
+    def __isLinked(self, move: tuple[int, int], currentColor: BoardState, player: PlayerOrder, size) -> bool:
+        """Return True if the given move is linked to both sides"""
+        cache = {}
+        liste = [move]
+        edgeLink1, edgeLink2 = False, False
+        while len(liste) > 0:
+            current = liste.pop()
+            if current in cache:
+                continue
+            cache[current] = True
+            if current[player] == 0:
+                edgeLink1 = True
+            if current[player] == size - 1:
+                edgeLink2 = True
+            if edgeLink1 and edgeLink2:
+                return True
+            for neighbor in hex_neighbors(current, (self.config.game.board_width, self.config.game.board_height)):
+                if self.board[neighbor] == currentColor:
+                    liste.append(neighbor)
+        return False
+
