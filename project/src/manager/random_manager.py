2f5614030dba53fca15757f1d85d647ce779aa6e
Arkazix <hugohamon27@gmail.com>
2024-02-01 11:34:13 +0100

    Add strategy, letter and number on board, strat mcts

--- project/src/manager/random_manager.py
+++ project/src/manager/random_manager.py
@@ -1,13 +1,14 @@
 from ..game.game import Game, BoardState
 from .manager import Manager
 from typing import Optional
+from ..config import Config
 from random import choice
 
 
 class RandomManager(Manager):
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, config: Config) -> None:
+        super().__init__(config)
 
     def get_move(self, game: Game) -> Optional[tuple[int, int]]:
         """Return the current move"""
