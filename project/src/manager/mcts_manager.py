4c3736b9c0a79b7ff2aa781ca4016ef4f4ee0df5
hugo-hamon <hugohamon27@gmail.com>
2024-02-03 13:35:16 +0100

    Add reset, undo, redo and pass command, more strategy to the page and fix id for css / html

--- project/src/manager/mcts_manager.py
+++ project/src/manager/mcts_manager.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
 from ..game.game import Game, BoardState
 from .manager import Manager
 from typing import Optional
 from ..config import Config
+from random import shuffle, choice
+import numpy as np
+import math
 
 
 class MCTSManager(Manager):
@@ -11,4 +15,8 @@ class MCTSManager(Manager):
 
     def get_move(self, game: Game) -> Optional[tuple[int, int]]:
         """Return the current move"""
+        pass
+
+    def reset(self) -> None:
+        """Reset the manager"""
         pass
