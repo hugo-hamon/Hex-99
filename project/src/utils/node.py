9dccc925a8aea1c27009fb4504494f122b931578
Pierre <pierrep070225@gmail.com>
2024-05-25 02:02:44 +0200

    VBSSS*

--- project/src/utils/node.py
+++ project/src/utils/node.py
@@ -5,46 +5,35 @@ from typing import Optional
 
 class Node:
 
-    def __init__(self, game: Game, value: float, children: Optional[list[Node]] = None) -> None:
+    def __init__(self, game: Game, parent: Optional[Node] = None, last_move: Optional[Node] = None, depth: int = 0) -> None:
         self.game = game
-        self.value = value
-        if children is None:
-            children = []
-        self.children = children
-        self.parent = None
+        self.alive = True
+        self.children = []
+        self.parent = parent
+        self.last_move = last_move
+        self.depth = depth
+        self.stale = False
 
-    # Request
+    # Requests
     def get_game(self) -> Game:
         """Return the game of the node"""
         return self.game
 
-    def get_value(self) -> float:
+    def get_parent(self) -> Node:
         """Return the value of the node"""
         return self.value
 
     def get_children(self) -> list[Node]:
         """Return the childrens of the node"""
-        return self.children
+        return self.parent
 
     # Command
-    def set_game(self, game: Game) -> None:
-        """Set the game of the node"""
-        self.game = game
-
-    def add_child(self, child: Node) -> None:
-        """Add a child to the node"""
-        self.children.append(child)
-
-    def set_value(self, value: float) -> None:
-        """Set the value of the node"""
-        self.value = value
-
     def expand(self) -> list[Node]:
         """Return all possible children of the node"""
-        for move in self.game.get_valid_moves(self.game.get_current_player()):
+        for move in self.game.get_turbo_valid_moves(self.game.get_current_player()):
             game_copy = self.game.copy()
             game_copy.move(move)
-            self.children.append(Node(game_copy, 0))    
+            self.children.append(Node(game_copy, self, move, self.depth - 1))    
         return self.children
     
     # Utils
@@ -60,3 +49,6 @@ class Node:
         print(f"{' ' * depth}{self.game.get_move_history()[-1][1]}: {self.value}")
         for child in self.children:
             child.display(depth + 1)
+
+    def __lt__(self, other: Node) -> bool:
+        return False
